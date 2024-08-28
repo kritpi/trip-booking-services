d55db97494d35e4a0bc06075a1fdd16715174d56
kritpi <kritpiruch@gmail.com>
2024-08-28 21:29:26 +0700

    Add: 1 Services Sample

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -1,23 +1,66 @@
-import { Task } from "../models/task";
-import { Request, Response } from "express";
+import { json, Request, Response } from "express";
+import { TaskService } from "../service/task";
 
-let tasks: Task[] = [];
-
+const taskService = new TaskService();
 export class TaskController {
-  getAllTask(req: Request, res: Response) {
-    res.status(200).json(tasks);
-  }
-  createTask(req: Request, res: Response) {
-    console.log("test", req.body.title)
-    const task: Task = {
-      id: tasks.length + 1,
-      title: req.body.title,
-      description: req.body.description,
-      status: false,
-    };
-    tasks.push(task);
-    res.json(tasks)
+  getAllTask = async (req: Request, res: Response) => {
+    try {
+      const tasks = await taskService.getAllTask();
+      res.status(200).json(tasks);
+    } catch (error) {
+      res.status(404).send("Not Found Tasks");
+    }
+  };
 
-  }
+  findTaskById = async (req: Request, res: Response) => {
+    try {
+      const taskId = parseInt(req.params.id);
+
+      const task = await taskService.findTaskById(taskId);
+      res.status(200).json(task);
+    } catch (error) {
+      res.status(404).send("Task Not Found");
+    }
+  };
+
+  createTask = async (req: Request, res: Response) => {
+    try {
+      const data = req.body;
+      await taskService.createTask(data);
+      res.status(201).send("Task Created");
+    } catch (error) {
+      res.status(400).send("Cannot Create Task");
+    }
+  };
+
+  editTask = async (req: Request, res: Response) => {
+    try {
+      const taskId = parseInt(req.params.id);
+      const data = req.body;
+
+      const oldTask = await taskService.findTaskById(taskId);
+      if (!oldTask) {
+        return res.status(404).send("Task Not Found");
+      }
+
+      oldTask.title = data.title || oldTask.title;
+      oldTask.description = data.description || oldTask.description;
+      oldTask.status = data.status || oldTask.status;
+
+      await taskService.editTask(oldTask);
+      res.status(200).send("Task Edited");
+    } catch (error) {
+      res.status(500).send("Internal Server Error");
+    }
+  };
+
+  deleteTask = async (req: Request, res: Response) => {
+    try {
+      const taskId = parseInt(req.params.id);
+      await taskService.deleteTask(taskId);
+      res.status(200).send("Task Deleted");
+    } catch (error) {
+      res.status(404).send("Can't Find Task");
+    }
+  };
 }
-
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -4,10 +4,11 @@ import { TaskController } from "../controllers/task";
 
 const router = Router();
 const taskController = new TaskController();
-router.get("/Test", (req: Request, res: Response) => {
-  res.send("Test Router Task");
-});
+
 router.get("/", taskController.getAllTask);
+router.get("/:id", taskController.findTaskById);
 router.post("/", taskController.createTask);
+router.put("/:id", taskController.editTask);
+router.delete("/:id", taskController.deleteTask);
 
 export default router;
