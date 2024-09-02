8c92fac366b7e323563c2b07b7b792e0e4bc864b
kritpi <kritpiruch@gmail.com>
2024-09-02 17:53:43 +0700

    Add cors package to make frontend to be able to integrate with services

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import taskRoutes from "./routes/task";
 import { Prisma, PrismaClient } from "@prisma/client";
+import cors from "cors"
 
 const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/tasks", taskRoutes);
 
